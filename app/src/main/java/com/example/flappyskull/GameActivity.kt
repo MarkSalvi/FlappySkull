26f55c57ed31385bfc7ad27c9bfc29ca7c905f3d
MarkSalvi <mark.salvi.19@gmail.com>
2022-04-26 22:17:20 +0200

    Aggiunto fragment_impostazioni.xml e fragmentlayout Aggiunta gameactivity e rimosso la topbar

--- app/src/main/java/com/example/flappyskull/GameActivity.kt
+++ app/src/main/java/com/example/flappyskull/GameActivity.kt
@@ -1,11 +1,37 @@
 package com.example.flappyskull
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+
+    val  Impostazioni = Impostazioni()
+    val btnImpostazioni = findViewById<ImageButton>(R.id.btnImpostazioni)
+        val leave = findViewById<ImageButton>(R.id.btnleave)
+
+    btnImpostazioni.setOnClickListener{
+        //the following code is used to set the fragment to the container
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flImpostazioni,Impostazioni)
+            addToBackStack(null)
+            commit()
+        }
+    }
+
+        leave.setOnClickListener(){
+            Intent(this, MainActivity::class.java).also{
+                startActivity(it)
+                finishAffinity()
+            }
+        }
+
     }
 }
